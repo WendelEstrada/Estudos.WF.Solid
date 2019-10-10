2bf4e43b3a0fb40b2188bea50130f61b19434a05
wendelestrada <wendel.estrada@t-systems.com.br>
2019-10-10 16:50:15 -0300

    Configuração do uso do SignalR para o Windows Forms

--- src/Estudos.WF.Solid.UI/Forms/FormPrincipal.Designer.cs
+++ src/Estudos.WF.Solid.UI/Forms/FormPrincipal.Designer.cs
@@ -31,15 +31,14 @@
             this.lblTitulo = new System.Windows.Forms.Label();
             this.btnGetAll = new System.Windows.Forms.Button();
             this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnConnectSignalR = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblTitulo
             // 
-            this.lblTitulo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.lblTitulo.AutoSize = true;
             this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitulo.Location = new System.Drawing.Point(303, 22);
+            this.lblTitulo.Location = new System.Drawing.Point(356, 9);
             this.lblTitulo.Name = "lblTitulo";
             this.lblTitulo.Size = new System.Drawing.Size(198, 29);
             this.lblTitulo.TabIndex = 0;
@@ -51,11 +50,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btnGetAll.Location = new System.Drawing.Point(351, 415);
             this.btnGetAll.Name = "btnGetAll";
-            this.btnGetAll.Size = new System.Drawing.Size(75, 23);
+            this.btnGetAll.Size = new System.Drawing.Size(213, 23);
             this.btnGetAll.TabIndex = 1;
             this.btnGetAll.Text = "Obter Todos";
             this.btnGetAll.UseVisualStyleBackColor = true;
-            this.btnGetAll.Click += new System.EventHandler(this.btnGetAll_Click);
+            this.btnGetAll.Click += new System.EventHandler(this.BtnGetAll_Click);
             // 
             // flowLayoutPanel
             // 
@@ -65,14 +64,25 @@
             this.flowLayoutPanel.AutoScroll = true;
             this.flowLayoutPanel.Location = new System.Drawing.Point(12, 66);
             this.flowLayoutPanel.Name = "flowLayoutPanel";
-            this.flowLayoutPanel.Size = new System.Drawing.Size(776, 343);
+            this.flowLayoutPanel.Size = new System.Drawing.Size(914, 343);
             this.flowLayoutPanel.TabIndex = 2;
+            // 
+            // btnConnectSignalR
+            // 
+            this.btnConnectSignalR.Location = new System.Drawing.Point(851, 415);
+            this.btnConnectSignalR.Name = "btnConnectSignalR";
+            this.btnConnectSignalR.Size = new System.Drawing.Size(75, 23);
+            this.btnConnectSignalR.TabIndex = 3;
+            this.btnConnectSignalR.Text = "Conectar";
+            this.btnConnectSignalR.UseVisualStyleBackColor = true;
+            this.btnConnectSignalR.Click += new System.EventHandler(this.BtnConnectSignalR_Click);
             // 
             // FormPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(938, 450);
+            this.Controls.Add(this.btnConnectSignalR);
             this.Controls.Add(this.flowLayoutPanel);
             this.Controls.Add(this.btnGetAll);
             this.Controls.Add(this.lblTitulo);
@@ -88,6 +98,7 @@
         private System.Windows.Forms.Label lblTitulo;
         private System.Windows.Forms.Button btnGetAll;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel;
+        private System.Windows.Forms.Button btnConnectSignalR;
     }
 }
 
--- src/Estudos.WF.Solid.UI/Forms/FormPrincipal.cs
+++ src/Estudos.WF.Solid.UI/Forms/FormPrincipal.cs
@@ -1,5 +1,6 @@
 ﻿using Estudos.WF.Solid.Core.Interfaces.CoreServices;
 using Estudos.WF.Solid.UI.Controls;
+using Microsoft.AspNet.SignalR.Client;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -15,6 +16,7 @@ namespace Estudos.WF.Solid.UI.Forms
     public partial class FormPrincipal : Form
     {
         private readonly ILutadorService _lutadorService;
+        private HubConnection _hubConnection;
 
         public FormPrincipal(ILutadorService lutadorService)
         {
@@ -23,7 +25,7 @@ namespace Estudos.WF.Solid.UI.Forms
             InitializeComponent();
         }
 
-        private void btnGetAll_Click(object sender, EventArgs e)
+        private void BtnGetAll_Click(object sender, EventArgs e)
         {
             var lutadores = _lutadorService.GetAll();
 
@@ -42,6 +44,36 @@ namespace Estudos.WF.Solid.UI.Forms
 
                 flowLayoutPanel.Controls.Add(lutadorUserControl);
             }
+        }
+
+        private void BtnConnectSignalR_Click(object sender, EventArgs e)
+        {
+            _hubConnection = new HubConnection("http://localhost:55235/signalr");
+            var hub = _hubConnection.CreateHubProxy("CompetidorHub");
+
+            Task teste = _hubConnection.Start().ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                    MessageBox.Show(task.Exception.Message);
+            });
+
+            teste.Wait();
+
+            hub.Invoke<string>("JoinTournament", "Rocky Balboa").ContinueWith(task => {
+                if (task.IsFaulted)
+                {
+                    Console.WriteLine("There was an error calling send: {0}",
+                                      task.Exception.GetBaseException());
+                }
+                else
+                {
+                    Console.WriteLine(task.Result);
+                }
+            });
+
+            hub.On<string>("DisplayMessageAll", param => {
+                Console.WriteLine(param);
+            });
         }
     }
 }
